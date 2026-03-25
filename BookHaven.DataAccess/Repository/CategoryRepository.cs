30b544c17a325792f9c225a84c6513aba864c644
ahmed <ahmed.skopljak.23@size.ba>
2026-03-25 18:30:43 +0100

    Repository for Category created, added Interfaces

---
+++ BookHaven.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq.Expressions;
+using BookHaven.DataAccess.Data;
+using BookHaven.DataAccess.Repository.IRepository;
+using BookHaven.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace BookHaven.DataAccess.Repository;
+
+public class CategoryRepository : Repository<Category>, ICategoryRepository
+{
+    private ApplicationDbContext _db;
+
+    public CategoryRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+    
+    public void Update(Category obj)
+    {
+        _db.Categories.Update(obj);
+    }
+
+    public void Save()
+    {
+        _db.SaveChanges();
+    }
+}
---
+++ BookHaven.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using BookHaven.Models;
+
+namespace BookHaven.DataAccess.Repository.IRepository;
+
+public interface ICategoryRepository : IRepository<Category>
+{
+    void Update(Category obj);
+    void Save(); 
+}
