9e7c6514f503b02e5705673dd1e4625183e04a25
ahmed <ahmed.skopljak.23@size.ba>
2026-05-07 16:22:00 +0200

    Tests: Product Repository Test

---
+++ BookHaven.Tests/ProductRepositoryTests.cs
@@ -0,0 +1,164 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BookHaven.DataAccess.Data;
+using BookHaven.DataAccess.Repository;
+using BookHaven.Models;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookHaven.Tests;
+
+public class ProductRepositoryTests
+{
+    //Database for every test
+    private async Task<ApplicationDbContext> GetDbContext()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+        var dbContext = new ApplicationDbContext(options);
+        dbContext.Database.EnsureCreated(); //This will seed my data into db
+        return dbContext;
+    }
+    
+    // Helper factory method to keep tests DRY (Don't Repeat Yourself)
+    private Product GetValidProductTemplate()
+    {
+        return new Product
+        {
+            Title = "Test Book",
+            Author = "Ahmed Skopljak",
+            Description = "Test Description",
+            ISBN = "123123ab",
+            ListPrice = 50,
+            Price = 45,
+            Price50 = 40,
+            Price100 = 35,
+            CategoryId = 2,
+            ImageUrl = ""
+        };
+    }
+
+    //Does new Product save in database
+    [Fact]
+    public async Task ProductRepository_Add_ShouldSaveProductToDB()
+    {
+        //ARANGE
+        var db = await GetDbContext();
+        var productRepository = new ProductRepository(db);
+
+        var product = GetValidProductTemplate();
+        
+        //ACT
+        productRepository.Add(product);
+        await db.SaveChangesAsync();
+        
+        //ASSERT
+        var result = await db.Products.FirstOrDefaultAsync(x => x.ISBN == "123123ab");
+
+        result.Should().NotBeNull();
+        result.Title.Should().Be("Test Book");
+        result.Price.Should().Be(45);
+    }
+    
+    //Boundary values Test
+    [Fact]
+    public async Task ProductRepository_Add_ShouldFail_WhenPriceIsBelowRange()
+    {
+        //ARANGE
+        var db = await GetDbContext();
+        var productRepository = new ProductRepository(db);
+        var productWithZeroPrice = new Product
+        {
+            Title = "Free Book",
+            Author = "Test Author",
+            Description = "Test Description",
+            ISBN = "000-000-120",
+            Price = 0, //Price 0
+            ListPrice = 0,
+            Price50 = 0,
+            Price100 = 0,
+            CategoryId = 1,
+            ImageUrl = ""
+        };
+        //ACT && ASSERT
+        productRepository.Add(productWithZeroPrice);
+        
+        Func<Task> act = async () => await db.SaveChangesAsync();
+
+        await act.Should().NotThrowAsync();
+    }
+
+    [Fact]
+    public void ProductModel_Validation_ShouldCatchError_WhenPriceIsZero()
+    {
+        //ARANGE
+        var product = new Product()
+        {
+            Title = "Free Book",
+            Author = "Test Author",
+            Description = "Test Description",
+            ISBN = "000-000-120",
+            Price = 0, //Price 0 [Range(1, 1000)]
+            ListPrice = 0,
+            Price50 = 0,
+            Price100 = 0,
+            CategoryId = 1,
+            ImageUrl = ""
+        };
+        
+        // ACT 
+        var context = new ValidationContext(product);
+        var results = new List<ValidationResult>();
+        var isValid = Validator.TryValidateObject(product, context, results, true);
+
+        // ASSERT 
+        isValid.Should().BeFalse();
+        results.Should().Contain(x => x.ErrorMessage.Contains("1 and 1000"));
+    }
+
+    [Fact]
+    public async Task ProductRepository_Update_ShouldModifyExistitngProduct()
+    {
+        //ARANGE
+        var db = await GetDbContext();
+        var productRepo = new ProductRepository(db);
+        
+        var productFromDB = db.Products.FirstOrDefault(x => x.Id == 1);
+        string updatedTitle = "Fortune of Time - Updated Title";
+        productFromDB.Title = updatedTitle;
+        productFromDB.Price = 95;
+        
+        //ACT
+        productRepo.Update(productFromDB);
+        await db.SaveChangesAsync();
+        
+        //ASSERT
+        var result = await db.Products.FindAsync(1);
+        
+        result.Should().NotBeNull();
+        result.Title.Should().Be(updatedTitle);
+        result.Price.Should().Be(95);
+        // Regression check: Ensure fields we didn't touch remain the same
+        result.Author.Should().Be("Billy Spark");
+    }
+
+    [Fact]
+    public async Task ProductRepository_Delete_ShouldDeleteProductFromDB()
+    {
+        //ARANGE
+        var db = await GetDbContext();
+        var productRepo = new ProductRepository(db);
+        
+        var productToDelete = db.Products.FirstOrDefault(x => x.Id == 2);
+        var initialCount = db.Products.Count();
+        
+        //ACT
+        productRepo.Remove(productToDelete);
+        await db.SaveChangesAsync();
+        
+        //ASSERT
+        var result = await db.Products.FindAsync(2);
+        var finalCount = await db.Products.CountAsync();
+        result.Should().BeNull();
+        finalCount.Should().Be(initialCount - 1);
+    }
+}
